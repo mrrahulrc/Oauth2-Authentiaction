517feec61f6e84811c2f27f636a1f6b97426bec3
rahul <rc333sam@gmail.com>
2024-02-15 19:08:28 +0530

    Add project files.

---
+++ OauthAuthentiaction/Constants/UnProtectedRoutes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OauthAuthentiaction.Constants
+{
+    public class UnProtectedRoutes
+    {
+        public static readonly List<String> routes = new List<String>()
+        {
+            "/",
+            "/home/index",
+            "/auth/oauth",
+            "/auth/token",
+            "/error/error"
+        };
+    }
+}
---
+++ OauthAuthentiaction/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OauthAuthentiaction.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult error()
+        {
+            return View();
+        }
+    }
+}
